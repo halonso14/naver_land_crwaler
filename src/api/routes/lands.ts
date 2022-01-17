edd302820f358d5c39b2dd1d9828c769bba500a3
halonso <hdsbsb@naver.com>
2022-01-17 11:50:11 +0900

    feat: implement GET /api/lands/real_estate

--- src/api/routes/lands.ts
+++ src/api/routes/lands.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response, NextFunction } from "express";
+import axios from 'axios';
 
 const route = Router();
 
@@ -6,16 +7,29 @@ export default (app: Router) => {
   app.use("/lands", route);
 
   route.get(
-    "/TBD",
+    "/real_estate",
 
     async (req: Request, res: Response, next: NextFunction) => {
-      console.log(`Calling Sign-Up endpoint with body: ${req.body}`);
+      const { id } = req.query;
+      let page: number = 1;
+      let list: any[] = [];
       try {
-        return res.status(200).json({ data: "TBD" });
+        while(true) {
+          const { data } = await axios.get(`https://m.land.naver.com/complex/getComplexArticleList?hscpNo=${id}&tradTpCd=A1&order=point_&showR0=N&page=${page}`)
+          if(data.result === 'error') {
+            return res.status(400).json({ data: "Invalid id." });
+          }
+          if(!data.result.list.length) {
+            break;
+          }
+          list = [ ...list, ...data.result.list];
+          page += 1;
+        }
       } catch (e) {
-        console.error(`Error from GET /lands/TBD: ${JSON.stringify}`);
+        console.error(`Unknown rrror from GET /lands/TBD: ${JSON.stringify(e)}`);
         return next(e);
       }
+      return res.status(200).json({ data: list });
     }
   );
 };
--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,7 +1,5 @@
 import expressLoader from "./express";
 
 export default async ({ expressApp }: any) => {
-  console.log("loader is called");
   await expressLoader({ app: expressApp });
-  console.log("✌️ Express loaded");
 };
